5fdd416224658b45a4980b73a8305f6040753742
elan33 <67664518@qq.com>
2019-03-13 15:01:05 +0800

    fix fingerprint bug

--- msm8953.mk
+++ msm8953.mk
@@ -23,8 +23,7 @@ PRODUCT_AAPT_CONFIG := normal
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # ANT+
 PRODUCT_PACKAGES += \
@@ -192,8 +191,8 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service
 
 # LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@1.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
